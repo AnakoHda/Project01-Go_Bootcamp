c185227c0f01e23e6ad5bbda300cca9a417cd708
Павел <87427150+AnakoHda@users.noreply.github.com>
2025-10-26 03:38:25 +0300

    Refactor 01

--- 01/main.go
+++ 01/main.go
@@ -10,39 +10,17 @@ import (
 func main() {
 	var a, b float64
 	var operand byte
+	scanner := bufio.NewScanner(os.Stdin)
 
 	fmt.Println("Enter the left operand: ")
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	text := scanner.Text()
-	a, err := strconv.ParseFloat(text, 64)
-	if err != nil {
-		fmt.Println("Invalid input", err)
-		return
-	}
+	a = readOperand(scanner)
 
 	fmt.Println("Enter the operator: ")
-	scanner.Scan()
-	text = scanner.Text()
-	if len(text) != 1 {
-		fmt.Println("Invalid input", text)
-		return
-	}
-	if text[0] != '+' && text[0] != '-' && text[0] != '*' && text[0] != '/' {
-		fmt.Println("Invalid input", text)
-		return
-	}
-	operand = text[0]
+	operand = readOperator(scanner)
 
 	fmt.Println("Enter the right operand: ")
-	scanner.Scan()
-	text = scanner.Text()
-	b, err = strconv.ParseFloat(text, 64)
-	// fmt.Println(text, a)
-	if err != nil {
-		fmt.Println("Invalid input", err)
-		return
-	}
+	b = readOperand(scanner)
+
 	fmt.Println("Result:")
 	switch operand {
 	case '+':
@@ -55,5 +33,36 @@ func main() {
 		fmt.Printf("%.3f\n", float64(a)/float64(b))
 	default:
 		fmt.Println("Invalid input", operand)
+	}
+}
+
+func readOperand(scanner *bufio.Scanner) float64 {
+	for {
+		scanner.Scan()
+		text := scanner.Text()
+		a, err := strconv.ParseFloat(text, 64)
+		if err != nil {
+			fmt.Println("Invalid input")
+			continue
+		}
+		return a
+	}
+}
+func readOperator(scanner *bufio.Scanner) byte {
+	for {
+		scanner.Scan()
+		text := scanner.Text()
+		if len(text) != 1 {
+			fmt.Println("Invalid input", text)
+			continue
+		}
+		switch text[0] {
+		case '+', '-', '*', '/':
+			return text[0]
+
+		default:
+			fmt.Println("Invalid input", text)
+			continue
+		}
 	}
 }
